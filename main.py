2450d04afaed51f47a5e825058e8802921992e33
Wiibleyde <nathan@bonnell.fr>
2022-11-11 14:51:01 +0100

    Clean version

--- main.py
+++ main.py
@@ -10,22 +10,7 @@ from socket import *
 from fcntl import ioctl
 from struct import pack
 
-def listAllIPs(address, netmask):
-    # Get all possible ip addresses of a network in a list
-    ip_list = []
-    for ip in IPv4Network(address + '/' + netmask, False):
-        ip_list.append(str(ip))
-    ip_list.remove(ip_list[0])
-    ip_list.remove(ip_list[-1])
-    return ip_list
-
-def getIpAdress(interface):
-    # Get the ip address of the interface
-    ip = scapy.get_if_addr(interface)
-    return ip
-
 def getNetworkMask(interface):
-    # Get the network mask of the interface
     s = socket(AF_INET, SOCK_DGRAM)
     netmask = ioctl(s.fileno(), 0x891b, pack('256s', interface[:15].encode('utf-8')))[20:24]
     return inet_ntoa(netmask)
@@ -39,14 +24,16 @@ def makeARPRequest(ip):
 
 def main():
     netmask = getNetworkMask(interface)
-    address = IPv4Network(getIpAdress(interface) + '/' + netmask, False)
+    address = IPv4Network(scapy.get_if_addr(interface) + '/' + netmask, False)
     ip = address.network_address
-    mac=[]
+    pcs=[]
     addressMask = address.prefixlen
     answered, unanswered = makeARPRequest(str(ip)+'/'+str(addressMask))
     for element in answered:
-        mac.append(element[1].hwsrc)
-    print(mac)
+        pcs.append([element[1].hwsrc, element[1].psrc])
+    pcs.sort(key=lambda x: [int (y) for y in x[1].split('.')])
+    for pc in pcs:
+        print(pc[0] + ' ' + pc[1])
 
 if __name__ == '__main__':
     main()
